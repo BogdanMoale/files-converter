cc765c80b172ba3b1c2b9be4e5696303f3c9647e
Bogdan <moale.bogdan.ionica@gmail.com>
2024-07-25 11:53:19 +0300

    initial

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export function ModeToggle() {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // Prevents rendering on the server
+  }
+
+  return (
+    <Button
+      variant={"ghost"}
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      {resolvedTheme === "dark" ? <SunIcon /> : <MoonIcon />}
+    </Button>
+  );
+}
